bac44d4f0716bb74daa7fe810c89eda45b97e03d
noeka <math570k@edu.ucl.dk>
2020-06-18 11:15:18 +0200

    Remove public directory and cookies

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
@@ -10,8 +9,6 @@ var app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", indexRouter);
 
